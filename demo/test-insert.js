0e5f55bd434a09ad3f99bf0ab7e665439c00b97e
Jack Martin <jerpdertin@gmail.com>
2021-10-10 09:42:10 +0100

    :memo: add README content

--- demo/test-insert.js
+++ demo/test-insert.js
@@ -45,15 +45,7 @@ Sed pellentesque ante quis nunc accumsan sodales. Nam vitae dui a quam bibendum 
   await collection.insertOne({
     _id: 'test',
     name: 'Dummy field',
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum dolor enim. Nullam mattis dolor faucibus mauris accumsan ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Nullam accumsan nisl ut nulla semper luctus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur justo lorem, laoreet nec convallis ac, varius non nisl. Phasellus iaculis consequat tellus, id venenatis diam euismod efficitur. Nulla ac tellus vitae augue pharetra convallis. Proin volutpat quam elit, id tristique neque interdum id. Aliquam enim magna, pretium ac purus et, hendrerit semper nunc.
-
-Aenean accumsan pretium odio, id euismod metus dapibus a. Morbi laoreet nibh a dui accumsan sollicitudin. Mauris mollis scelerisque felis, ac dignissim est bibendum id. Fusce ut leo leo. Cras ultricies turpis nec nisi imperdiet laoreet. Maecenas mattis risus id lectus interdum congue. Sed euismod venenatis ipsum, a porta mauris tincidunt et. Cras rutrum scelerisque molestie.
-
-Suspendisse nec metus in est fringilla rhoncus. Maecenas nec arcu dignissim, blandit sapien eu, congue massa. Phasellus nec nibh sed nisi iaculis sollicitudin quis eu risus. Mauris a semper libero. Maecenas sodales a libero euismod lobortis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce a turpis mattis, tempus nunc vitae, mollis erat. Morbi mattis turpis eget luctus varius. Sed at ex nulla. Fusce feugiat dignissim sem sed eleifend. Fusce eget sem maximus justo tincidunt malesuada eu at magna.
-
-Sed pellentesque ante quis nunc accumsan sodales. Nam vitae dui a quam bibendum tempor ut at est. Ut laoreet, turpis nec vestibulum pulvinar, ipsum magna fermentum sem, non pretium purus magna quis nisi. Vestibulum condimentum tortor vitae ex dictum, quis faucibus libero vulputate. Pellentesque viverra malesuada lorem nec sodales. Maecenas quis elementum neque, id elementum neque. Morbi molestie sapien non nisl faucibus dapibus.
-
-`,
+    description: `Data`,
   });
   console.log('insert col1');
   await collection.insertMany(dummyData);
