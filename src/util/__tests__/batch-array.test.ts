d31c0ba7604b9bd29a79563307d0fdb577682b88
Jack Martin <jerpdertin@gmail.com>
2021-10-16 09:53:13 +0100

    :white_check_mark: add tests for batchArray

--- src/util/__tests__/batch-array.test.ts
+++ src/util/__tests__/batch-array.test.ts
@@ -1,3 +1,21 @@
-it('works', () => {
-  expect(1).toBe(1);
+import { batchArray } from '../batch-array';
+
+it('works with a simple dataset', () => {
+  const testArray = [1,2,3,4,5,6,7,8,9,10];
+  const expectedOutput =  [ [ 1, 2 ], [ 3, 4 ], [ 5, 6 ], [ 7, 8 ], [ 9, 10 ] ];
+  const batchedArray = batchArray(testArray, 2);
+  expect(batchedArray).toEqual(expectedOutput);
+});
+
+it('handles outliers correctly', () => {
+  const testArray = [1];
+  const expectedOutput =  [ [1] ];
+  const batchedArray = batchArray(testArray, 2);
+  expect(batchedArray).toEqual(expectedOutput);
+});
+it('handles empty arrays correctly', () => {
+  const testArray: Array<number> = [];
+  const expectedOutput: Array<number> =  [ ];
+  const batchedArray = batchArray(testArray, 2);
+  expect(batchedArray).toEqual(expectedOutput);
 });
