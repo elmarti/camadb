0a0fb0d76d07a59eee8451bc9457e07980914bf5
elmarti <jerpdertin@gmail.com>
2023-04-13 16:47:05 +0100

    :white_check_mark: collection meta tests

--- src/modules/persistence/inmemory/__tests__/collection-meta.test.ts
+++ src/modules/persistence/inmemory/__tests__/collection-meta.test.ts
@@ -1,3 +1,29 @@
-it('works', () => {
-  expect(1).toBe(1);
+import { CollectionMeta } from '../collection-meta';
+
+describe('CollectionMeta', () => {
+  let collectionMeta: CollectionMeta;
+
+  beforeEach(() => {
+    collectionMeta = new CollectionMeta();
+  });
+
+  it('should create an instance of CollectionMeta', () => {
+    expect(collectionMeta).toBeInstanceOf(CollectionMeta);
+  });
+
+  it('should return undefined on calling init', async () => {
+    const metaStructure = { collectionName: 'test', columns: [], indexes: [] };
+
+    expect(await collectionMeta.init('test', metaStructure)).toBeUndefined();
+  });
+
+  it('should return undefined when no meta data is set', async () => {
+    expect(await collectionMeta.get()).toBeUndefined();
+  });
+
+  it('should update the meta value', async () => {
+    const metaStructure = { collectionName: 'test', columns: [], indexes: [] };
+    await collectionMeta.update('test', metaStructure);
+    expect(await collectionMeta.get()).toBeUndefined()
+  });
 });
