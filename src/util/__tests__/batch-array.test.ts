4ceff492436188df6fbc4e4c013f8872abf10bae
elmarti <jerpdertin@gmail.com>
2023-04-13 16:47:49 +0100

    :white_check_mark: util tests

--- src/util/__tests__/batch-array.test.ts
+++ src/util/__tests__/batch-array.test.ts
@@ -1,21 +1,41 @@
+// batchArray.test.ts
+
 import { batchArray } from '../batch-array';
 
-it('works with a simple dataset', () => {
-  const testArray = [1,2,3,4,5,6,7,8,9,10];
-  const expectedOutput =  [ [ 1, 2 ], [ 3, 4 ], [ 5, 6 ], [ 7, 8 ], [ 9, 10 ] ];
-  const batchedArray = batchArray(testArray, 2);
-  expect(batchedArray).toEqual(expectedOutput);
-});
+describe('batchArray', () => {
+  it('should return the input array as a single batch if its length is less than or equal to the batchLength', () => {
+    const inputArray = [1, 2, 3, 4, 5];
+    const batchLength = 5;
+    const expectedResult = [[1, 2, 3, 4, 5]];
 
-it('handles outliers correctly', () => {
-  const testArray = [1];
-  const expectedOutput =  [ [1] ];
-  const batchedArray = batchArray(testArray, 2);
-  expect(batchedArray).toEqual(expectedOutput);
-});
-it('handles empty arrays correctly', () => {
-  const testArray: Array<number> = [];
-  const expectedOutput: Array<number> =  [ ];
-  const batchedArray = batchArray(testArray, 2);
-  expect(batchedArray).toEqual(expectedOutput);
+    expect(batchArray(inputArray, batchLength)).toEqual(expectedResult);
+  });
+
+  it('should return the input array as multiple batches if its length is greater than the batchLength', () => {
+    const inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const batchLength = 5;
+    const expectedResult = [
+      [1, 2, 3, 4, 5],
+      [6, 7, 8, 9, 10],
+    ];
+
+    expect(batchArray(inputArray, batchLength)).toEqual(expectedResult);
+  });
+
+  it('should return the input array in batches of the default batchLength when no batchLength is provided', () => {
+    const inputArray = Array.from({ length: 15000 }, (_, i) => i + 1);
+    const expectedResult = [
+      Array.from({ length: 10000 }, (_, i) => i + 1),
+      Array.from({ length: 5000 }, (_, i) => i + 10001),
+    ];
+
+    expect(batchArray(inputArray)).toEqual(expectedResult);
+  });
+
+  it('should return an empty array when the input array is empty', () => {
+    const inputArray: number[] = [];
+    const expectedResult: number[][] = [];
+
+    expect(batchArray(inputArray)).toEqual(expectedResult);
+  });
 });
