5977e6931e462ab1d8d59298a55fe1bd53cf3dca
elmarti <jerpdertin@gmail.com>
2023-04-13 16:46:21 +0100

    :white_check_mark: module tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,3 +1,27 @@
-it('works', () => {
-  expect(1).toBe(1);
+// cama.test.ts
+
+import { Cama, Collection } from '../';
+import { ICamaConfig } from '../interfaces/cama-config.interface';
+import { PersistenceAdapterEnum } from '../interfaces/perisistence-adapter.enum';
+
+describe('Cama', () => {
+  const camaConfig: ICamaConfig = {
+    persistenceAdapter: PersistenceAdapterEnum.InMemory,
+    path: 'test-db',
+  };
+
+
+
+  it('should initialize a collection', async () => {
+    const cama = new Cama(camaConfig);
+    const collectionName = 'testCollection';
+    const collectionConfig = {
+      columns: [],
+      indexes: []
+    };
+
+    const collection = await cama.initCollection(collectionName, collectionConfig);
+    expect(collection).toBeDefined();
+    expect(collection).toBeInstanceOf(Collection);
+  });
 });
