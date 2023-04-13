7e5bbc493facb4ebf06d7c824c1f6da99046b486
elmarti <jerpdertin@gmail.com>
2023-04-13 16:46:34 +0100

    :white_check_mark: logging tests

--- src/modules/logger/__tests__/loglevel.test.ts
+++ src/modules/logger/__tests__/loglevel.test.ts
@@ -1,3 +1,40 @@
-it('works', () => {
-  expect(1).toBe(1);
+import { LoglevelLogger } from "../loglevel";
+import { ICamaConfig } from "../../../interfaces/cama-config.interface";
+import { LogLevel } from "../../../interfaces/logger-level.enum";
+import { PersistenceAdapterEnum } from "../../../interfaces/perisistence-adapter.enum";
+
+describe("LoglevelLogger", () => {
+  let logger: LoglevelLogger;
+  const camaConfig: ICamaConfig = {
+    logLevel: LogLevel.Debug,
+    persistenceAdapter: PersistenceAdapterEnum.InMemory
+  };
+
+  beforeEach(() => {
+    logger = new LoglevelLogger(camaConfig);
+  });
+
+  it("should set the log level if it is provided in the config", () => {
+    expect(logger["logger"].getLevel()).toEqual(1);
+  });
+
+  it("should not set the log level if it is not provided in the config", () => {
+    const newLogger = new LoglevelLogger({
+      persistenceAdapter: PersistenceAdapterEnum.InMemory
+    });
+    expect(newLogger["logger"]).toBeUndefined();
+  });
+
+  it("should log a message with the specified log level", () => {
+    const spy = jest.spyOn(logger["logger"], "debug");
+    logger.log(LogLevel.Debug, "test");
+    expect(spy).toHaveBeenCalledWith({ level: LogLevel.Debug, message: "test" });
+  });
+
+  it("should start and end a timer", () => {
+    const pointer = logger.startTimer();
+    expect(pointer).toEqual("");
+    logger.endTimer(LogLevel.Debug, pointer, "test");
+    // check that the timer was started and stopped
+  });
 });
